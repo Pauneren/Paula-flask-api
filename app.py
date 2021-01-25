6e1769f65d20fc7134340b3229898ddef49bb5a7
Paula Nerenberg <paulita.glam@gmail.com>
2021-01-25 15:11:42 +0000

    bugfixing

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-import datetime
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -12,34 +11,29 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    return response
+app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=10)
-app.secret_key = 'oursupersecretabracadabrahocuspocus'
+app.secret_key = 'jose'
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
